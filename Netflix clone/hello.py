75829f9cf7ef0bcd42edabb4eeb1ab192fb09020
Rakib Hossain <143281573+rkrrakib@users.noreply.github.com>
2024-01-29 12:44:14 +0600

    Add files via upload

---
+++ Netflix clone/hello.py
@@ -0,0 +1,13 @@
+import time
+import pyautogui
+count = 5
+print('T minus')
+for i in range(5):
+    print(count)
+    time.sleep(1)
+    count = count - 1
+    for i in range (10)
+    pyautogui.typewrite('hello man')
+    pyautogui.press('enter')
+    time.sleep(.3)
+    
